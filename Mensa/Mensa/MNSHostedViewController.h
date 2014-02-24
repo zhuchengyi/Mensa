c8b5ccb02a4a5e2ac845cf5fd522d30ab8d4dbcf
Jordan Kay <jordanekay@twitter.com>
2014-02-23 16:31:47 -0800

    Require -[MNSHostedviewController viewForObject:] to call super

--- Mensa/Mensa/MNSHostedViewController.h
+++ Mensa/Mensa/MNSHostedViewController.h
@@ -12,7 +12,7 @@
 - (void)selectObject:(id)object;
 - (BOOL)canSelectObject:(id)object;
 - (void)setViewHighlighted:(BOOL)highlighted forObject:(id)object;
-- (UIView *)viewForObject:(id)object;
+- (UIView *)viewForObject:(id)object __attribute__((objc_requires_super));
 
 + (NSArray *)reuseIdentifiers;
 + (NSString *)reuseIdentifierForObject:(id)object;
