dbe0265d7df64160bd1ee81c567d913abc70e424
jordanekay <jordanekay@mac.com>
2015-10-31 15:46:11 -0700

    Move variant enum declaration to beginning

--- MensaExample/PrimeFlagView.swift
+++ MensaExample/PrimeFlagView.swift
@@ -10,13 +10,13 @@ import Mensa
 import UIKit.UIView
 
 class PrimeFlagView: UIView {
-    @IBOutlet private(set) weak var textLabel: UILabel?
-    private(set) var formatString: String!
-    
     enum Style: Int {
         case Default
         case Compact
     }
+
+    @IBOutlet private(set) weak var textLabel: UILabel?
+    private(set) var formatString: String!
     
     // MARK: NSObject
     override func awakeFromNib() {
